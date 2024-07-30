9b92608e9e948c816659aa1b7861fd2079c89075
rodrigodamiao <saskydamiao@gmail.com>
2024-07-30 15:04:53 -0300

    primeiro commit

---
+++ main.java
@@ -0,0 +1,17 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("salve");
+
+        String s = "salve";
+        int count = 0;
+        int number = 10;
+        for(int i = 0; i<s.length(); i++){
+            System.out.println(s.charAt(i));
+        }
+
+        while(count < number){
+            count++;
+            System.out.println(count);
+        }
+    }
+}
